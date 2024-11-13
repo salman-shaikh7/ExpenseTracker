79aab94b4103ba00b03921b52fddecd20b1fc2b2
Saumya Uttam <uttamsaumya12@gmail.com>
2024-11-12 19:28:33 -0500

    modified Application DB

--- ExpenseTracker/Data/ApplicationDbContext.cs
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -16,16 +16,5 @@ namespace ExpenseTracker.Data
         public DbSet <PaymentMethod> paymentMethods { get; set; }
         public DbSet <Category> categories { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Category>().HasData(
-        //        new Category { CategoryId = 1, Name = "Food" },
-        //        new Category { CategoryId = 1, Name = "Shopping" },
-        //        new Category { CategoryId = 1, Name = "Fuel" },
-        //        new Category { CategoryId = 1, Name = "Rent" }
-
-        //        );
-
-        //}
     }
 }
